3c62189e52f69c610c89d76d8127212eca200492
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-09-12 16:28:48 +0200

    Trying out different things

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
-alert('Usa il pop up per dare dei messaggi');
-window.alert('Sto studiando JS');
+alert("Usa il pop up per dare dei messaggi");
+window.alert("Sto studiando JS");
--- js/naming.js
+++ js/naming.js
@@ -3,14 +3,22 @@
 
 /*var pet = 'gatto';
 var Pet = 'cane';*/
-var petPreferito = 'criceto';
-var PetPreferito = 'coniglio';
+var petPreferito = "criceto";
+var PetPreferito = "coniglio";
 
-var $pet = 'giraffa';
+var $pet = "giraffa";
 console.log($pet);
-var _pet = 'leone';
+var _pet = "leone";
 console.log(_pet);
-var _pet2 = 'pantera';
-document.write(_pet2);
+/*var _pet2 = "pantera";
+document.write(_pet2);*/
 
+document.getElementById("giraffe-pic").innerHTML = $pet;
+document.getElementById("hamster").innerHTML = petPreferito;
+document.getElementById("rabbit").innerHTML = PetPreferito;
+
+function changeColor(newColor) {
+  const elem = (document.getElementById("giraffe").style.backgroundColor =
+    newColor);
+}
 //var 12 = 'numero';
